27317e982d4ef59a2c80e55c195c5162a3f25da3
naeemraza19 <117772397+naeemraza19@users.noreply.github.com>
2025-01-22 09:38:24 +0500

    landing page design (#5)
    * landing page design
    
    * conflicts
    
    ---------
    
    Co-authored-by: Hafiz Temuri <3893769+temurih@users.noreply.github.com>

--- src/app/components/SocialMedia.tsx
+++ src/app/components/SocialMedia.tsx
@@ -7,14 +7,17 @@ import { mergeClassNames } from './utils/mergeClassNames';
 interface SocialMediaProps {
   path?: string;
   className?: string;
+  customStyles?: React.CSSProperties; // Add custom styles prop
 }
 
-const SocialMedia: React.FC<SocialMediaProps> = ({ path = '', className }) => {
+const SocialMedia: React.FC<SocialMediaProps> = ({ path = '', className, customStyles }) => {
   return (
-    <div className={mergeClassNames(styles['social-media'], className)}>
+    <div
+      className={mergeClassNames(styles['social-media'], className)}
+      style={customStyles} // Apply dynamic styles here
+    >
       <a target="_blank" href="https://www.instagram.com/_mappetizer">
         <Image
-          className={styles['social-media']}
           src={assetUrl(`/images${path}/social-media/instagram.png`)}
           alt="Instagram"
           title="Instagram"
@@ -24,7 +27,6 @@ const SocialMedia: React.FC<SocialMediaProps> = ({ path = '', className }) => {
       </a>
       <a target="_blank" href="https://www.tiktok.com/@mappetizer">
         <Image
-          className={styles['social-media']}
           src={assetUrl(`/images${path}/social-media/tiktok.png`)}
           alt="TikTok"
           title="TikTok"
@@ -37,7 +39,6 @@ const SocialMedia: React.FC<SocialMediaProps> = ({ path = '', className }) => {
         href="https://www.linkedin.com/company/mappetizer/about"
       >
         <Image
-          className={styles['social-media']}
           src={assetUrl(`/images${path}/social-media/linkedin.png`)}
           alt="LinkedIn"
           title="LinkedIn"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,256 @@
-import React from 'react';
+'use client';
 import Image from 'next/image';
 import styles from './page.module.scss';
 import { assetUrl } from './components/utils';
+import React, { useRef, useState } from 'react';
+import SocialMedia from './components/SocialMedia';
 
-const Home: React.FC = () => {
+const style: React.CSSProperties = {
+  backgroundImage: `linear-gradient(rgba(95, 93, 63, 0.20), rgba(95, 93, 63, 0.20)), url('${assetUrl(
+    '/images/map-pattern.png',
+  )}')`,
+};
+
+// Function to scroll the slider
+const scrollSlider = (
+  direction: number,
+  setCurrentIndex: React.Dispatch<React.SetStateAction<number>>,
+  currentIndex: number,
+  slides: any[],
+) => {
+  const slider = document.getElementById('horizontalSlider');
+  if (slider) {
+    const scrollAmount = 300; // Adjust based on your slide width
+    slider.scrollBy({ left: direction * scrollAmount, behavior: 'smooth' });
+    const newIndex = currentIndex + direction;
+    if (newIndex >= 0 && newIndex < slides.length) {
+      setCurrentIndex(newIndex);
+    }
+  }
+};
+
+const slides = [
+  {
+    title: 'SAY HI',
+    description: 'Meet the team behind this ambitious app build.',
+    img: '/images/1.jpg',
+  },
+  {
+    title: 'THE WAITLIST',
+    description: 'Early birds only. Sign up to be among the first app users.',
+    img: '/images/2.jpg',
+  },
+  {
+    title: 'RESTO ROLL CALL',
+    description: 'We’re requesting feedback from any and all restaurants.',
+    img: '/images/3.jpg',
+  },
+  {
+    title: 'FOLLOW US',
+    description: <SocialMedia customStyles={{justifyContent: 'flex-start'}} />,
+    img: '/images/4.jpg',
+  },
+  {
+    title: 'TELL A FRIEND',
+    description:
+      'Support our vision by sharing it with your friends far and wide.',
+    img: '/images/5.jpg',
+  },
+  {
+    title: 'GET INVOLVED',
+    description: 'Join the movement and make a difference.',
+    img: '/images/6.jpg',
+  },
+  {
+    title: 'PARTNER WITH US',
+    description: 'Explore opportunities to collaborate.',
+    img: '/images/7.jpg',
+  },
+  {
+    title: 'OUR MISSION',
+    description: 'Learn more about what drives our vision.',
+    img: '/images/8.jpg',
+  },
+  {
+    title: 'TEAM UPDATES',
+    description: 'Catch up on the latest updates from our team.',
+    img: '/images/9.jpg',
+  },
+  {
+    title: 'THANK YOU',
+    description: 'We appreciate your support and enthusiasm!',
+    img: '/images/10.jpg',
+  },
+];
+
+export default function Home() {
+  const sectionTop = useRef<HTMLDivElement>(null);
+  const sectionBottom = useRef<HTMLDivElement>(null);
+  const sectionFooter = useRef<HTMLDivElement>(null);
+  const [showInputSection, setShowInputSection] = useState(false);
+
+  // State to track the current index of the slider
+  const [currentIndex, setCurrentIndex] = useState(0);
+
   return (
-    <div className={styles['main-wrapper']}>
-      <header>
-        <Image
-          className={styles.logo}
-          src={assetUrl('/images/logo.png')}
-          alt="Mappetizer"
-          title="Mappetizer"
-          width={150}
-          height={20.5}
-        />
-        <h4 className={styles['coming-soon']}>COMING SOON</h4>
-      </header>
-      <div className={styles['text-wrapper']}>
-        <h1>PAUSE.</h1>
-        <h3>
-          WE ARE UPDATING OUR SITE.
-          <br />
-          CHECK BACK EARLY FEB.
-        </h3>
+    <div>
+      <div className={styles.hero}>
+        {/* Video Background */}
+        <video
+          autoPlay
+          loop
+          muted
+          playsInline
+          className={styles.videoBackground}
+        >
+          <source src="/video.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+
+        {/* Content on Top of the Video */}
+        <div className={styles.header}>
+          <Image
+            className={styles.logo}
+            src={assetUrl('/images/logo.png')}
+            alt="Mappetizer"
+            title="Mappetizer"
+            width={150}
+            height={40}
+          />
+          {!showInputSection && (
+            <h1
+              className={styles['center-text']}
+              onClick={() => setShowInputSection(!showInputSection)}
+              style={{ cursor: 'pointer' }}
+            >
+              JOIN THE WAITLIST
+            </h1>
+          )}
+          <h1 className={styles['right-text']}>COMING SOON</h1>
+        </div>
+        <div className={styles['header-line']} />
+
+        {showInputSection && (
+          <h1
+            className={styles.waitlist}
+            onClick={() => setShowInputSection(!showInputSection)}
+          >
+            JOIN THE WAITLIST
+          </h1>
+        )}
+
+        {/* Middle section */}
+        <div className={styles.middleSection}>
+          <h1>
+            <span>DISCOVER</span> <br />
+            <span>HIDDEN GEMS</span> <br />
+            <span>IN YOUR CITY</span>
+          </h1>
+          {showInputSection && (
+            <div className={styles.inputSection}>
+              <input type="text" placeholder="NAME" className={styles.input} />
+              <input
+                type="email"
+                placeholder="EMAIL"
+                className={styles.input}
+              />
+              <button className={styles.button}>SUBMIT</button>
+            </div>
+          )}
+        </div>
+      </div>
+
+      <div className={styles.textSection}>
+        <p className={styles['text-section-description']}>
+          <span>Discover Restaurants, Book Tables,</span> <br />
+          <span>Share Experiences</span> <br />
+        </p>
+      </div>
+
+      {/* Horizontal Slider Section */}
+      <div ref={sectionBottom} className={styles.sliderSection}>
+        <h1 className={styles.sectionTitle}>SECOND COURSE</h1>
+        <div className={styles.sliderWrapper}>
+          {/* Next Button */}
+          <button
+            className={`${styles.navButton1} ${
+              currentIndex < slides.length - 1 ? '' : styles.disabled1
+            }`}
+            onClick={() =>
+              scrollSlider(1, setCurrentIndex, currentIndex, slides)
+            }
+            disabled={currentIndex >= slides.length - 1}
+          >
+            <Image
+              className={styles.navIcon}
+              src={assetUrl('/images/next.png')}
+              alt="Next"
+              title="Next"
+              width={20}
+              height={20}
+            />
+          </button>
+
+          <div id="horizontalSlider" className={styles.horizontalSlider}>
+            {slides.map((slide, index) => (
+              <div key={index} className={styles.sliderItem}>
+                <Image
+                  className={styles.sliderImage}
+                  src={slide.img}
+                  alt="restaurant"
+                  title="Restaurant"
+                  width={300}
+                  height={230}
+                />
+                <h3 className={styles.sliderTitle}>{slide.title}</h3>
+                <p className={styles.sliderDescription}>{slide.description}</p>
+              </div>
+            ))}
+          </div>
+
+          {/* Back Button */}
+          <button
+            className={`${styles.navButton} ${
+              currentIndex > 0 ? '' : styles.disabled
+            }`}
+            onClick={() =>
+              scrollSlider(-1, setCurrentIndex, currentIndex, slides)
+            }
+            disabled={currentIndex <= 0}
+          >
+            <Image
+              className={styles.navIcon}
+              src={assetUrl('/images/back.png')}
+              alt="Back"
+              title="Back"
+              width={20}
+              height={20}
+            />
+          </button>
+        </div>
+      </div>
+
+      <div ref={sectionFooter} className={styles.footer} style={style}>
+        <div className={styles.footerContent}>
+          <h2 className={styles.footerItem}>WORK WITH US</h2>
+          <h2 className={styles.footerItem}>CEO LINKEDIN</h2>
+          <h2 className={styles.footerItem}>PRESS INQUIRIES</h2>
+          <h2 className={styles.footerItem}>CONTACT</h2>
+          <h2 className={styles.footerItem}>FAQ</h2>
+        </div>
+        <div className={styles['footer-line']} />
+        <div className={styles.footerSection}>
+          <Image
+            className={styles.logo}
+            src={assetUrl('/images/logo.png')}
+            alt="Mappetizer"
+            title="Mappetizer"
+            width={150}
+            height={40}
+          />
+          <SocialMedia />
+        </div>
       </div>
     </div>
   );
-};
-
-export default Home;
+}
