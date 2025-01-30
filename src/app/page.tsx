598cb49f762f5329ef410209d869c5c7f11338cf
naeemraza19 <muhammadnaeemraza19@gmail.com>
2025-01-30 07:07:57 +0500

    Design Changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -53,7 +53,18 @@ const slides = [
   {
     title: 'FOLLOW US',
     description: (
-      <SocialMedia customStyles={{ justifyContent: 'flex-start' }} />
+      <div>
+        <SocialMedia
+          instagramUrl="https://www.instagram.com/_mappetizer"
+          instagramIcon="/images/social-media/instagramBlack.png"
+          tiktokUrl="https://www.tiktok.com/@mappetizer"
+          tiktokIcon="/images/social-media/tiktokBlack.png"
+          linkedinUrl="https://www.linkedin.com/company/mappetizer/about"
+          linkedinIcon="/images/social-media/linkedinBlack.png"
+          className="custom-class"
+          customStyles={{ justifyContent: 'flex-start' }}
+        />
+      </div>
     ),
     img: '/images/4.png',
   },
@@ -69,8 +80,17 @@ export default function Home() {
   const sectionTop = useRef<HTMLDivElement>(null);
   const sectionBottom = useRef<HTMLDivElement>(null);
   const sectionFooter = useRef<HTMLDivElement>(null);
-  const [showInputSection, setShowInputSection] = useState(false);
-  const [isMobile, setIsMobile] = useState(false);
+  const [showInputSection, setShowInputSection] = useState<boolean>(false);
+  const [isMobile, setIsMobile] = useState<boolean>(false);
+  const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+  const [hoveredFooter, setHoveredFooter] = useState<string | null>(null);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [pressInquiries, setPressInquiries] = useState<boolean>(false);
+  const [isContact, setIsContact] = useState<boolean>(false);
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
 
   useEffect(() => {
     const handleResize = () => {
@@ -166,15 +186,15 @@ export default function Home() {
       <div>
         <div className={styles.textSection}>
           <p className={styles['text-section-description']}>
-            <span>Discover Restaurants, Book Tables,</span> <br />
-            <span>Share Experiences</span> <br />
+            <span>Discover restaurants. Book tables.</span> <br />
+            <span>Share experiences.</span> <br />
           </p>
         </div>
         <div className={styles['text-section-line']} />
       </div>
 
       {/* Horizontal Slider Section */}
-      <div ref={sectionBottom} className={styles.sliderSection}>
+      <div className={styles.sliderSection}>
         <h1 className={styles.sectionTitle}>SECOND COURSE</h1>
         <div className={styles.sliderWrapper}>
           {/* Next Button */}
@@ -198,26 +218,214 @@ export default function Home() {
           </button>
 
           <div id="horizontalSlider" className={styles.horizontalSlider}>
-            {slides.map((slide, index) => (
-              <div key={index} className={styles.sliderItem}>
-                <Image
-                  className={styles.sliderImage}
-                  src={slide.img}
-                  alt="restaurant"
-                  title="Restaurant"
-                  width={300}
-                  height={230}
-                />
-                <h3 className={styles.sliderTitle}>{slide.title}</h3>
-                <div className={styles.sliderDescription}>
-                  {typeof slide.description === 'string' ? (
-                    <p>{slide.description}</p>
-                  ) : (
-                    slide.description
-                  )}
+            {isModalOpen && (
+              <div
+                className={styles.modalOverlay}
+                role="dialog"
+                aria-labelledby="modal-title"
+              >
+                <div className={styles.modalContent}>
+                  <div className={styles.inputSection}>
+                    <input
+                      type="text"
+                      placeholder="Your Preferred Role"
+                      className={styles.input}
+                    />
+                    <input
+                      type="text"
+                      placeholder="Your Email"
+                      className={styles.input}
+                    />
+                    <input
+                      type="text"
+                      placeholder="First Name"
+                      className={styles.input}
+                    />
+                    <input
+                      type="text"
+                      placeholder="Last Name"
+                      className={styles.input}
+                    />
+                    <input
+                      type="text"
+                      placeholder="Pronouns"
+                      className={styles.input}
+                    />
+                    <input
+                      type="text"
+                      placeholder="Your City"
+                      className={styles.input}
+                    />
+                    <input
+                      type="text"
+                      placeholder="Why do you want to work with us?"
+                      className={styles.input}
+                    />
+                    <input
+                      type="text"
+                      placeholder="How did you hear about us?"
+                      className={styles.input}
+                    />
+                    <button className={styles.input}>
+                      Attach your portfolio
+                    </button>
+                    <div className={styles.buttonSection}>
+                      <button className={styles.button}>Submit</button>
+                      <button onClick={closeModal} className={styles.button}>
+                        Cancel
+                      </button>
+                    </div>
+                    <h3 className={styles.modalText}>
+                      If a role becomes available in your preferred category and
+                      if we think you might be a good fit, we will reach out via
+                      email.
+                    </h3>
+                  </div>
                 </div>
               </div>
-            ))}
+            )}
+            {!isMobile &&
+              slides.map((slide, index) => (
+                <div
+                  key={index}
+                  className={styles.sliderItem}
+                  onMouseEnter={() => setHoveredIndex(index)}
+                  onMouseLeave={() => setHoveredIndex(null)}
+                >
+                  <Image
+                    className={styles.sliderImage}
+                    src={slide.img}
+                    alt="restaurant"
+                    title="Restaurant"
+                    width={300}
+                    height={230}
+                  />
+                  {slide.title === 'FOLLOW US' || hoveredIndex !== index ? (
+                    <h3 className={styles.sliderTitle}>{slide.title}</h3>
+                  ) : slide.title === 'SAY HI' ? (
+                    <div>
+                      <button
+                        className={`${styles.sliderTitle} ${styles.titleButton}`}
+                        onClick={() => {
+                          window.location.href = 'https://codereroute.com';
+                        }}
+                      >
+                        {slide.title}
+                      </button>
+                    </div>
+                  ) : slide.title === 'THE WAITLIST' ? (
+                    <div>
+                      <button
+                        className={`${styles.sliderTitle} ${styles.titleButton}`}
+                        onClick={() => {
+                          window.scrollTo({ top: 0, behavior: 'smooth' });
+                        }}
+                      >
+                        {slide.title}
+                      </button>
+                    </div>
+                  ) : slide.title === 'RESTO ROLL CALL' ? (
+                    <div>
+                      <button
+                        className={`${styles.sliderTitle} ${styles.titleButton}`}
+                        onClick={() => {
+                          setIsModalOpen(true);
+                        }}
+                      >
+                        {slide.title}
+                      </button>
+                    </div>
+                  ) : (
+                    <div>
+                      <button
+                        className={`${styles.sliderTitle} ${styles.titleButton}`}
+                      >
+                        {slide.title}
+                      </button>
+                    </div>
+                  )}
+                  <div className={styles.sliderDescription}>
+                    {typeof slide.description === 'string' ? (
+                      <p>{slide.description}</p>
+                    ) : (
+                      slide.description
+                    )}
+                  </div>
+                </div>
+              ))}
+
+            {isMobile &&
+              slides.map((slide, index) => (
+                <div
+                  key={index}
+                  className={styles.sliderItem}
+                  onMouseEnter={() => setHoveredIndex(index)}
+                  onMouseLeave={() => setHoveredIndex(null)}
+                >
+                  <Image
+                    className={styles.sliderImage}
+                    src={slide.img}
+                    alt="restaurant"
+                    title="Restaurant"
+                    width={300}
+                    height={230}
+                  />
+                  {slide.title === 'FOLLOW US' ? (
+                    <h3 className={styles.sliderTitle}>{slide.title}</h3>
+                  ) : slide.title === 'SAY HI' ? (
+                    <div>
+                      <button
+                        className={styles.titleButton}
+                        onClick={() => {
+                          window.location.href = 'https://codereroute.com';
+                        }}
+                      >
+                        {slide.title}
+                      </button>
+                    </div>
+                  ) : slide.title === 'THE WAITLIST' ? (
+                    <div>
+                      <button
+                        className={styles.titleButton}
+                        onClick={() => {
+                          window.scrollTo({ top: 0, behavior: 'smooth' });
+                        }}
+                      >
+                        {slide.title}
+                      </button>
+                    </div>
+                  ) : slide.title === 'RESTO ROLL CALL' ? (
+                    <div>
+                      <button
+                        className={styles.titleButton}
+                        onClick={() => {
+                          setIsModalOpen(true);
+                        }}
+                      >
+                        {slide.title}
+                      </button>
+                    </div>
+                  ) : (
+                    <div>
+                      <button
+                        className={styles.titleButton}
+                        onClick={() => {
+                          setIsModalOpen(true);
+                        }}
+                      >
+                        {slide.title}
+                      </button>
+                    </div>
+                  )}
+                  <div className={styles.sliderDescription}>
+                    {typeof slide.description === 'string' ? (
+                      <p>{slide.description}</p>
+                    ) : (
+                      slide.description
+                    )}
+                  </div>
+                </div>
+              ))}
           </div>
 
           {/* Back Button */}
@@ -246,12 +454,105 @@ export default function Home() {
         {!isMobile && (
           <>
             <div className={styles.footerContent}>
-              <h2 className={styles.footerItem}>WORK WITH US</h2>
-              <h2 className={styles.footerItem}>CEO LINKEDIN</h2>
-              <h2 className={styles.footerItem}>PRESS INQUIRIES</h2>
-              <h2 className={styles.footerItem}>CONTACT</h2>
-              <h2 className={styles.footerItem}>FAQ</h2>
+              <div
+                className={styles.footerItemWrapper}
+                onMouseEnter={() => setHoveredFooter('WORK WITH US')}
+                onMouseLeave={() => setHoveredFooter(null)}
+              >
+                {hoveredFooter === 'WORK WITH US' ? (
+                  <a
+                    target="_blank"
+                    href={'https://codereroute.com/'}
+                    rel="noopener noreferrer"
+                  >
+                    <button
+                      className={`${styles.footerItem} ${styles.footerButton}`}
+                    >
+                      WORK WITH US
+                    </button>
+                  </a>
+                ) : (
+                  <h2 className={styles.footerItem}>WORK WITH US</h2>
+                )}
+              </div>
+
+              <div
+                className={styles.footerItemWrapper}
+                onMouseEnter={() => setHoveredFooter('CEO LINKEDIN')}
+                onMouseLeave={() => setHoveredFooter(null)}
+              >
+                {hoveredFooter === 'CEO LINKEDIN' ? (
+                  <a
+                    target="_blank"
+                    href={
+                      'https://www.linkedin.com/in/danielle-dufour?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app'
+                    }
+                    rel="noopener noreferrer"
+                  >
+                    <button
+                      className={`${styles.footerItem} ${styles.footerButton}`}
+                    >
+                      CEO LINKEDIN
+                    </button>
+                  </a>
+                ) : (
+                  <h2 className={styles.footerItem}>CEO LINKEDIN</h2>
+                )}
+              </div>
+
+              {!pressInquiries && (
+                <div
+                  className={styles.footerItemWrapper}
+                  onMouseEnter={() => setHoveredFooter('PRESS INQUIRIES')}
+                  onMouseLeave={() => setHoveredFooter(null)}
+                >
+                  {hoveredFooter === 'PRESS INQUIRIES' ? (
+                    <button
+                      className={`${styles.footerItem} ${styles.footerButton}`}
+                      onClick={() => setPressInquiries(!pressInquiries)}
+                    >
+                      PRESS INQUIRIES
+                    </button>
+                  ) : (
+                    <h2 className={styles.footerItem}>PRESS INQUIRIES</h2>
+                  )}
+                </div>
+              )}
+
+              {pressInquiries && (
+                <div className={styles.emailContainer}>
+                  <a href="mailto:hello@mappetizer.com">press@mappetizer.com</a>
+                </div>
+              )}
+
+              {!isContact && (
+                <div
+                  className={styles.footerItemWrapper}
+                  onMouseEnter={() => setHoveredFooter('CONTACT')}
+                  onMouseLeave={() => setHoveredFooter(null)}
+                >
+                  {hoveredFooter === 'CONTACT' ? (
+                    <button
+                      className={`${styles.footerItem} ${styles.footerButton}`}
+                      onClick={() => setIsContact(!isContact)}
+                    >
+                      CONTACT
+                    </button>
+                  ) : (
+                    <h2 className={styles.footerItem}>CONTACT</h2>
+                  )}
+                </div>
+              )}
+
+              {isContact && (
+                <div className={styles.emailContainer}>
+                  <a href="mailto:hello@mappetizer.com">
+                    contact@mappetizer.com
+                  </a>
+                </div>
+              )}
             </div>
+
             <div className={styles['footer-line']} />
             <div className={styles.footerSection}>
               <Image
@@ -262,21 +563,64 @@ export default function Home() {
                 width={150}
                 height={40}
               />
-              <SocialMedia />
+              {pressInquiries && (
+                <div className={styles.footerItemWrapper}>
+                  <button
+                    className={`${styles.footerItem} ${styles.footerButton}`}
+                    onClick={() => {
+                      setPressInquiries(!pressInquiries);
+                      setHoveredFooter(null);
+                    }}
+                  >
+                    PRESS INQUIRIES
+                  </button>
+                </div>
+              )}
+              {isContact && (
+                <div className={styles.footerItemWrapper}>
+                  <button
+                    className={`${styles.footerItem} ${styles.footerButton}`}
+                    onClick={() => {
+                      setIsContact(!isContact);
+                      setHoveredFooter(null);
+                    }}
+                  >
+                    CONTACT
+                  </button>
+                </div>
+              )}
+              <div>
+                <SocialMedia
+                  instagramUrl="https://www.instagram.com/_mappetizer"
+                  instagramIcon="/images/social-media/instagram.png"
+                  tiktokUrl="https://www.tiktok.com/@mappetizer"
+                  tiktokIcon="/images/social-media/tiktok.png"
+                  linkedinUrl="https://www.linkedin.com/company/mappetizer/about"
+                  linkedinIcon="/images/social-media/linkedin.png"
+                  className="custom-class"
+                />
+              </div>
             </div>
           </>
         )}
         {isMobile && (
           <>
             <div className={styles.socialMediaContainer}>
-              <SocialMedia />
+              <SocialMedia
+                instagramUrl="https://www.instagram.com/_mappetizer"
+                instagramIcon="/images/social-media/instagram.png"
+                tiktokUrl="https://www.tiktok.com/@mappetizer"
+                tiktokIcon="/images/social-media/tiktok.png"
+                linkedinUrl="https://www.linkedin.com/company/mappetizer/about"
+                linkedinIcon="/images/social-media/linkedin.png"
+                className="custom-class"
+              />
             </div>
             <div className={styles.footerButtonContainer}>
               <button className={styles.footerButton}>WORK HERE</button>
               <button className={styles.footerButton}>CEO LINKEDIN</button>
               <button className={styles.footerButton}>PRESS INQUIRIES</button>
               <button className={styles.footerButton}>CONTACT</button>
-              <button className={styles.footerButton}>FAQ</button>
             </div>
             <div className={styles.footerLogo}>
               <Image
