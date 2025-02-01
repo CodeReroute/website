446cd1a9ccd41fb169fc94e65829ccbb0bf861cc
naeemraza19 <muhammadnaeemraza19@gmail.com>
2025-02-02 01:11:22 +0500

    resto-roll-form

--- src/app/components/restoModal/RestoModal.tsx
+++ src/app/components/restoModal/RestoModal.tsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { mergeClassNames } from '../utils/mergeClassNames';
 import styles from './RestoModal.module.scss';
 import CloseIcon from './CloseIcon';
+import { webConfig } from '../utils/webConfig';
+import ReCaptchaV3, { requestRecaptchaV3Token } from '../utils/ReCaptchaV3';
+
+interface BaseResponse {
+  success: boolean;
+  message?: string;
+  error?: string;
+}
 
 interface RestoModalProps {
   className?: string;
@@ -9,12 +17,104 @@ interface RestoModalProps {
 }
 
 const RestoModal: React.FC<RestoModalProps> = ({ closeModal, className }) => {
-  const handleSubmit = (e: React.FormEvent) => {
+  const [firstName, setFirstName] = useState<string>('');
+  const [lastName, setLastName] = useState<string | undefined>('');
+  const [email, setEmail] = useState<string | undefined>('');
+  const [location, setLocation] = useState<string | undefined>('');
+  const [restaurantName, setRestaurantName] = useState<string | undefined>('');
+  const [marketingTools, setMarketingTools] = useState<string | undefined>('');
+  const [reservationTools, setReservationTools] = useState<string | undefined>(
+    '',
+  );
+  const [customerSource, setCustomerSource] = useState<string | undefined>('');
+  const [challenges, setChallenges] = useState<string | undefined>('');
+  const [frustrations, setFrustrations] = useState<string | undefined>('');
+  const [hasMarketingAgency, setHasMarketingAgency] = useState<
+    string | undefined
+  >('');
+  const [marketingBudgetMetrics, setMarketingBudgetMetrics] = useState<
+    string | undefined
+  >('');
+  const [softwareSpend, setSoftwareSpend] = useState<string | undefined>('');
+  const [hasInfluencers, setHasInfluencers] = useState<string | undefined>('');
+  const [hideNegativeReviews, setHideNegativeReviews] = useState<
+    string | undefined
+  >('');
+  const [desiredFeatures, setDesiredFeatures] = useState<string | undefined>(
+    '',
+  );
+
+  const [resp, setResp] = useState<BaseResponse | null>(null);
+
+  useEffect(() => {
+    const formattedName = firstName.split(' ');
+    if (formattedName.length === 4) {
+      setLastName(formattedName[formattedName.length - 1]);
+    } else {
+      setLastName(undefined);
+    }
+  }, [firstName]);
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+    setResp(null);
+
+    await requestRecaptchaV3Token(async (token) => {
+      if (!token) {
+        alert('reCAPTCHA verification failed. Please try again.');
+        return;
+      }
+
+      // Use the token directly in the API request
+      const response = await fetch(
+        `${webConfig.nextPublicBaseUrl}/user-info/create?token=${token}`,
+        {
+          method: 'POST',
+          body: JSON.stringify({
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            types: ['RESTAURANT'],
+            feedback: {
+              location: location,
+              restaurantName: restaurantName,
+              marketingTools: marketingTools,
+              reservationTools: reservationTools,
+              customerSource: customerSource,
+              challenges: challenges,
+              frustrations: frustrations,
+              hasMarketingAgency: hasMarketingAgency,
+              marketingBudgetMetrics: marketingBudgetMetrics,
+              softwareSpend: softwareSpend,
+              hasInfluencers: hasInfluencers,
+              hideNegativeReviews: hideNegativeReviews,
+              desiredFeatures: desiredFeatures,
+            },
+          }),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        },
+      );
+
+      const data = await response.json();
+      if (response.ok) {
+        console.log(resp?.message, 'response');
+        setResp({
+          success: true,
+          message: 'Application submitted successfully!',
+        });
+      } else {
+        setResp({
+          success: false,
+          error: data.message || 'Something went wrong. Please try again.',
+        });
+      }
+    });
   };
 
   return (
     <form className={className} onSubmit={handleSubmit}>
+      <ReCaptchaV3 />
       <div className={styles.closeIconContainer}>
         <CloseIcon onClick={closeModal} />
       </div>
@@ -27,6 +127,8 @@ const RestoModal: React.FC<RestoModalProps> = ({ closeModal, className }) => {
       <div className={styles.inputRow}>
         <div className={styles.inputSection}>
           <input
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
             type="text"
             placeholder="Name *"
             className={styles.input}
@@ -36,6 +138,8 @@ const RestoModal: React.FC<RestoModalProps> = ({ closeModal, className }) => {
 
         <div className={styles.inputSection}>
           <input
+            value={restaurantName}
+            onChange={(e) => setRestaurantName(e.target.value)}
             type="text"
             placeholder="Restaurant name *"
             className={styles.input}
@@ -48,6 +152,8 @@ const RestoModal: React.FC<RestoModalProps> = ({ closeModal, className }) => {
       <div className={styles.inputRow}>
         <div className={styles.inputSection}>
           <input
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
             type="text"
             placeholder="Location *"
             className={styles.input}
@@ -57,6 +163,8 @@ const RestoModal: React.FC<RestoModalProps> = ({ closeModal, className }) => {
 
         <div className={styles.inputSection}>
           <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             type="email"
             placeholder="Email *"
             className={styles.input}
@@ -74,7 +182,12 @@ const RestoModal: React.FC<RestoModalProps> = ({ closeModal, className }) => {
             What tools or platforms does your restaurant you use to manage
             marketing?
           </label>
-          <input type="text" className={styles.input} />
+          <input
+            type="text"
+            className={styles.input}
+            value={marketingTools}
+            onChange={(e) => setMarketingTools(e.target.value)}
+          />
         </div>
 
         <div
@@ -84,7 +197,12 @@ const RestoModal: React.FC<RestoModalProps> = ({ closeModal, className }) => {
             What tools or platforms does your restaurant you use to manage
             reservations?
           </label>
-          <input type="text" className={styles.input} />
+          <input
+            type="text"
+            className={styles.input}
+            value={reservationTools}
+            onChange={(e) => setReservationTools(e.target.value)}
+          />
         </div>
       </div>
 
@@ -96,7 +214,12 @@ const RestoModal: React.FC<RestoModalProps> = ({ closeModal, className }) => {
           <label className={styles.inputLabel}>
             Where do customers typically hear about your restaurant?
           </label>
-          <input type="text" className={styles.input} />
+          <input
+            type="text"
+            className={styles.input}
+            value={customerSource}
+            onChange={(e) => setCustomerSource(e.target.value)}
+          />
         </div>
 
         <div
@@ -106,7 +229,12 @@ const RestoModal: React.FC<RestoModalProps> = ({ closeModal, className }) => {
             What challenges does your restaurant face for maintaining a
             consistent flow of customers?
           </label>
-          <input type="text" className={styles.input} />
+          <input
+            type="text"
+            className={styles.input}
+            value={challenges}
+            onChange={(e) => setChallenges(e.target.value)}
+          />
         </div>
       </div>
 
@@ -118,7 +246,12 @@ const RestoModal: React.FC<RestoModalProps> = ({ closeModal, className }) => {
             What frustrates you the most about your current customer engagement
             or marketing efforts?
           </label>
-          <input type="text" className={styles.input} />
+          <input
+            type="text"
+            className={styles.input}
+            value={frustrations}
+            onChange={(e) => setFrustrations(e.target.value)}
+          />
         </div>
 
         <div
@@ -127,7 +260,12 @@ const RestoModal: React.FC<RestoModalProps> = ({ closeModal, className }) => {
           <label className={styles.inputLabel}>
             Do you work with a marketing agency?
           </label>
-          <input type="text" className={styles.input} />
+          <input
+            type="text"
+            className={styles.input}
+            value={hasMarketingAgency}
+            onChange={(e) => setHasMarketingAgency(e.target.value)}
+          />
         </div>
         <div
           className={mergeClassNames(styles.inputSection, styles.fullRowInput)}
@@ -136,7 +274,12 @@ const RestoModal: React.FC<RestoModalProps> = ({ closeModal, className }) => {
             If you were to spend $1,000 on marketing, how would you determine
             whether it was effective?
           </label>
-          <input type="text" className={styles.input} />
+          <input
+            type="text"
+            className={styles.input}
+            value={marketingBudgetMetrics}
+            onChange={(e) => setMarketingBudgetMetrics(e.target.value)}
+          />
         </div>
         <div
           className={mergeClassNames(styles.inputSection, styles.fullRowInput)}
@@ -144,7 +287,12 @@ const RestoModal: React.FC<RestoModalProps> = ({ closeModal, className }) => {
           <label className={styles.inputLabel}>
             How much are you currently spending on software per month?
           </label>
-          <input type="text" className={styles.input} />
+          <input
+            type="text"
+            className={styles.input}
+            value={softwareSpend}
+            onChange={(e) => setSoftwareSpend(e.target.value)}
+          />
         </div>
         <div
           className={mergeClassNames(styles.inputSection, styles.fullRowInput)}
@@ -152,7 +300,12 @@ const RestoModal: React.FC<RestoModalProps> = ({ closeModal, className }) => {
           <label className={styles.inputLabel}>
             Do you work with influencers or local food bloggers?
           </label>
-          <input type="text" className={styles.input} />
+          <input
+            type="text"
+            className={styles.input}
+            value={hasInfluencers}
+            onChange={(e) => setHasInfluencers(e.target.value)}
+          />
         </div>
         <div
           className={mergeClassNames(styles.inputSection, styles.fullRowInput)}
@@ -160,7 +313,12 @@ const RestoModal: React.FC<RestoModalProps> = ({ closeModal, className }) => {
           <label className={styles.inputLabel}>
             Would you like it if negative reviews were not made public?
           </label>
-          <input type="text" className={styles.input} />
+          <input
+            type="text"
+            className={styles.input}
+            value={hideNegativeReviews}
+            onChange={(e) => setHideNegativeReviews(e.target.value)}
+          />
         </div>
         <div
           className={mergeClassNames(styles.inputSection, styles.fullRowInput)}
@@ -169,7 +327,12 @@ const RestoModal: React.FC<RestoModalProps> = ({ closeModal, className }) => {
             If you could design the perfect tool to help run your restaurant,
             what features would it have?
           </label>
-          <input type="text" className={styles.input} />
+          <input
+            type="text"
+            className={styles.input}
+            value={desiredFeatures}
+            onChange={(e) => setDesiredFeatures(e.target.value)}
+          />
         </div>
       </div>
 
@@ -181,10 +344,27 @@ const RestoModal: React.FC<RestoModalProps> = ({ closeModal, className }) => {
         >
           CANCEL
         </button>
-        <button className={styles.button} type="submit">
-          SUBMIT
+        <button
+          onClick={handleSubmit}
+          className={`${styles.button} ${resp?.success ? styles.success : ''}`}
+        >
+          {resp && resp.success ? 'SUBMITTED' : 'SUBMIT'}
         </button>
       </div>
+      {resp && (
+        <div>
+          <p
+            style={{
+              color: resp.success ? '#d0f29b' : 'red',
+              fontWeight: '700',
+              fontSize: '16px',
+              textAlign: 'center',
+            }}
+          >
+            {resp.success ? resp.message : resp.error}
+          </p>
+        </div>
+      )}
     </form>
   );
 };
