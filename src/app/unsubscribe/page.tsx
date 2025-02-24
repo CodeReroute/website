2f5177a2a98207956d4c4043864e0d5c9c81e6f7
usmanchlorine <usman.chh2001@gmail.com>
2025-02-24 10:12:48 +0500

    suggested changes

--- src/app/unsubscribe/page.tsx
+++ src/app/unsubscribe/page.tsx
@@ -8,14 +8,11 @@ import Image from 'next/image';
 import { assetUrl } from '../components/utils';
 import { useApiRequest } from '../components/utils/hooks/useApi';
 
-
 const UNSUBSCRIBE_ENDPOINT = 'unsubscribe';
-const BASE_URL='https://mappetizer.ca/monitoring/v1/email'
+const BASE_URL = 'https://mappetizer.ca/monitoring/v1/email';
 const Unsubscribe: React.FC = () => {
   const searchParams = useSearchParams();
   const [email] = useState<string | null>(searchParams.get('email') || null);
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
   const [unsubscribeState, makeUnsubscribeRequest] = useApiRequest();
 
   useEffect(() => {
@@ -26,18 +23,6 @@ const Unsubscribe: React.FC = () => {
       );
     }
   }, [email, makeUnsubscribeRequest]);
-
-  useEffect(() => {
-    if (unsubscribeState.type === 'REQUEST_ERROR') {
-      console.log(unsubscribeState.error);
-      setError('Invalid response from the server');
-      setLoading(false);
-    } else if (unsubscribeState.type === 'REQUEST_SUCCESS') {
-      console.log(unsubscribeState.data);
-      setError(null);
-      setLoading(false);
-    }
-  }, [unsubscribeState]);
 
   return (
     <>
@@ -55,18 +40,22 @@ const Unsubscribe: React.FC = () => {
       </div>
       <div className={styles['main-container']}>
         <div>
-          {loading && <p className={styles['main-heading']}>Loading...</p>}
-          {error && <p className={styles['main-heading']}>Error: {error}</p>}
-          {!loading &&
-            !error &&
-            unsubscribeState.type === 'REQUEST_SUCCESS' && (
-              <>
-                <p className={styles['main-heading']}>
-                  You have been unsubscribed from Mappetizer emails
-                </p>
-                <p className={styles['main-email']}>{email}</p>
-              </>
-            )}
+          {unsubscribeState.type === 'REQUEST_START' && (
+            <p className={styles['main-heading']}>Loading...</p>
+          )}
+          {unsubscribeState.type === 'REQUEST_ERROR' && (
+            <p className={styles['main-heading']}>
+              Error: {unsubscribeState.error.message}
+            </p>
+          )}
+          {unsubscribeState.type === 'REQUEST_SUCCESS' && (
+            <>
+              <p className={styles['main-heading']}>
+                You have been unsubscribed from Mappetizer emails
+              </p>
+              <p className={styles['main-email']}>{email}</p>
+            </>
+          )}
         </div>
       </div>
       <Footer />
